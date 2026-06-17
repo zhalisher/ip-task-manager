5095bdbfdd501e2021110a36a1df7e2bd88d3919
Alisher <z.alish.zh@gmail.com>
2026-06-17 22:20:58 +0500

    feat: add helper function for handler

---
+++ internal/delivery/http/handler/responce.go
@@ -0,0 +1,12 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondJSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
